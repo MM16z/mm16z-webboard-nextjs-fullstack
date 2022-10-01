e5b7c3258c83bc6184ea21ac03c259495bcae1b4
MM16z <imm0rz55160@gmail.com>
2022-10-01 08:36:05 +0700

    .#1Prebuild Ongoing MM16

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../assets/sass/_app.scss";
+import "../assets/sass/pages_style/home.scss";
+import "../assets/sass/sub_components_style/navbar.scss";
+import "../assets/sass/sub_components_style/post-box-container.scss";
+
+import Navbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="_app">
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/login/index.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div className="login-page-container">
+            
+        </div>
+    );
+}
+
+export default Login;
